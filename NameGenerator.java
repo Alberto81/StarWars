b338189cf16077d53baff44259a12ef95475a5e0
Alberto Antelo <albertoantelopresa@gmail.com>
2014-04-07 09:41:51 +0200

    cambiados numeros por constantes

--- NameGenerator.java
+++ NameGenerator.java
@@ -8,15 +8,19 @@
 public class NameGenerator
 {
     // no hace falta ni constructor ni atributos
+    public static final int CUT_PRIM_AP=3;
+    public static final int CUT_NOMBRE=3;
+    public static final int CUT_SEG_AP=2;
+    public static final int CUT_CITY_BORN=3;
+    public static final int PRIMERA_LETRA = 0;
     
-
     /**
      *introduces datos por parametro y devuelve un nombreStarWars
      */
     public String generateStarWarsName(String primAp, String nombre, String segAp, String cityNac)
     {
-        String name = primAp.substring(0,3) + nombre.substring(0,3);
-        String apellido = segAp.substring(0,2)+ cityNac.substring(0,3);
+        String name = primAp.substring(PRIMERA_LETRA,CUT_PRIM_AP) + nombre.substring(PRIMERA_LETRA,CUT_NOMBRE);
+        String apellido = segAp.substring(PRIMERA_LETRA,CUT_SEG_AP)+ cityNac.substring(PRIMERA_LETRA,CUT_CITY_BORN);
         return name +" "+ apellido;
     }
     
